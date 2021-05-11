241e95d31eefaa45a2ec9d9961e94f6b7ad4faa8
kamilsmr <kamils6035@gmail.com>
2021-05-11 16:55:21 +0300

    uptade

--- src/data.js
+++ src/data.js
@@ -1,31 +1,37 @@
 export default [
-    {
-        id: 1,
-        name:'kamil SAYIN',
-        age:35,
-        image:'https://pbs.twimg.com/profile_images/989464158373601281/oTLXAjNu.jpg',
-
-    },
-    {
-        id: 2,
-        name:'Ahmet SELIM',
-        age:23,
-        image:'https://img.tozlu.com/Uploads/UrunResimleri/Thumb/tek-cepli-erkek-t-shirt-f288.jpg',
-
-    },
-    {
-        id: 3,
-        name:'Esma MARTI',
-        age:30,
-        image:'https://i.pinimg.com/originals/75/1e/d2/751ed259364bdf3e9f41dd0ef94e68b3.jpg',
-
-    },
-    {
-        id: 4,
-        name:'Mert OKAY',
-        age:19,
-        image:'https://i.pinimg.com/originals/f6/78/6a/f6786a193673c6b54f526618fe58d21d.jpg',
-    },
-
-
+  {
+    id: 1,
+    name: 'Bertie Yates',
+    age: 29,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+  },
+  {
+    id: 2,
+    name: 'Hester Hogan',
+    age: 32,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+  },
+  {
+    id: 3,
+    name: 'Larry Little',
+    age: 36,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+  },
+  {
+    id: 4,
+    name: 'Sean Walsh',
+    age: 34,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+  },
+  {
+    id: 5,
+    name: 'Lola Gardner',
+    age: 29,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+  },
 ];
